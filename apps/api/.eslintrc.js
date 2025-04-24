b7d2c45769b1bef9d7fd8baf2650a5f7cf33c38d
Rushik-B <rushikbusiness@gmail.com>
2025-04-23 17:47:15 -0700

    Resolved dependency issues

---
+++ apps/api/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ["next/core-web-vitals"],
+  // You can add custom rules or overrides here if needed
+  // e.g.:
+  // rules: {
+  //   "some-rule": "warn"
+  // }
+}; 
